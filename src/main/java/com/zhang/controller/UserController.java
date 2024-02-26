6ed189a661832c6eb08d1b3dec3a5ae229bd3e04
clumsy <3144550951@qq.com>
2024-02-26 12:45:06 +0800

    work4

--- src/main/java/com/zhang/controller/UserController.java
+++ src/main/java/com/zhang/controller/UserController.java
@@ -23,13 +23,6 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-/*    @GetMapping("/user")
-    public result getCurrentUser() {
-        Authentication authentication= SecurityContextHolder.getContext().getAuthentication();
-        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-        String username = userDetails.getUsername();
-        return result.OK(userDetails);
-    }*/
 
     /**
      * 注册方法
